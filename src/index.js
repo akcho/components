fcfdfb4859d3fa160d6184685b1de51991f03785
Andrew Cho <andrew@augmentedrealitycapture.com>
2020-08-14 13:37:19 -0700

    commit to master

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" msg="Hi" />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" msg="Bye" />
-      <CommentDetail author="Jane" timeAgo="Today at 5:00PM" msg="Why" />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" msg="Hi" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 2:00AM" msg="Bye" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Today at 5:00PM" msg="Why" />
+      </ApprovalCard>
     </div>
   );
 };
